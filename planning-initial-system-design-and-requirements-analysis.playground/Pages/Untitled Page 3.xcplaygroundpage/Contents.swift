0c1855c12a8360996244742f99ecf4644f94587a
Kylta <Christophe.bugnon02@gmail.com>
2019-04-24 16:22:41 +0200

    The 'FeedViewController' doesn't know what will be used as FeedLoader then we can use what we want depending on the class conform to. The VC doesn't know all the logic. We change the behavior of the code without changing the VC. Everything now is really easy to tests and we can continue with sustainability and confidence.

--- planning-initial-system-design-and-requirements-analysis.playground/Pages/Untitled Page 3.xcplaygroundpage/Contents.swift
+++ planning-initial-system-design-and-requirements-analysis.playground/Pages/Untitled Page 3.xcplaygroundpage/Contents.swift
@@ -1,7 +1,64 @@
 //: [Previous](@previous)
 
-import Foundation
+//: [Previous](@previous)
 
-var str = "Hello, playground"
+import UIKit
 
-//: [Next](@next)
+protocol FeedLoader {
+    func loadFeed(completion: @escaping ([String]) -> Void)
+}
+
+class FeedViewController: UIViewController {
+    var loader: FeedLoader!
+
+    convenience init(loader: FeedLoader) {
+        self.init()
+        self.loader = loader
+    }
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        loader.loadFeed { loadedItems in
+            // Update UI
+        }
+    }
+}
+
+class RemoteFeedLoader: FeedLoader {
+    func loadFeed(completion: @escaping ([String]) -> Void) {
+        // do something
+    }
+}
+
+class LocalFeedLoader: FeedLoader {
+    func loadFeed(completion: @escaping ([String]) -> Void) {
+        // do something
+    }
+}
+
+struct Reachability {
+    static let networkAvailable = false
+}
+
+class RemoteWithLocalFallbackFeedLoader: FeedLoader {
+    let remote: RemoteFeedLoader
+    let local: LocalFeedLoader
+
+    init(remote: RemoteFeedLoader, local: LocalFeedLoader) {
+        self.remote = remote
+        self.local = local
+    }
+
+    func loadFeed(completion: @escaping ([String]) -> Void) {
+        if Reachability.networkAvailable {
+            remote.loadFeed { loadedItems in
+                // do something
+            }
+        } else {
+            local.loadFeed { loadedItems in
+                // do something
+            }
+        }
+    }
+}
