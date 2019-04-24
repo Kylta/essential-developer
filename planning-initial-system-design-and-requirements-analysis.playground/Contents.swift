25f6ffcac156c9ba6c89f8a60948e56dcde6b067
Kylta <Christophe.bugnon02@gmail.com>
2019-04-24 15:20:50 +0200

    Creating FeedViewController whose inherit from UIViewController, so the module depends on UIKit. We use a closure as a signature for update UI and we are asynchronous

--- planning-initial-system-design-and-requirements-analysis.playground/Contents.swift
+++ planning-initial-system-design-and-requirements-analysis.playground/Contents.swift
@@ -1,3 +1,18 @@
 import UIKit
 
-var str = "Hello, playground"
+class FeedViewController: UIViewController {
+    var loadFeed: ((([String]) -> Void) -> Void)!
+
+    convenience init(loadFeed: @escaping (([String]) -> Void) -> Void) {
+        self.init()
+        self.loadFeed = loadFeed
+    }
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        loadFeed { loadedItems in
+            // Update UI
+        }
+    }
+}
