84c8ff0b496a92132bc83c073d73447f7d9d764e
Kylta <Christophe.bugnon02@gmail.com>
2019-04-25 12:07:27 +0200

    Creates test for one tracker. Check if succeed and failed login return messages

--- CleanArchitectureComposition/CleanArchitectureCompositionTests/LoginUseCaseOutputComposerTests.swift
+++ CleanArchitectureComposition/CleanArchitectureCompositionTests/LoginUseCaseOutputComposerTests.swift
@@ -10,16 +10,61 @@ import XCTest
 @testable import CleanArchitectureComposition
 
 class LoginUseCaseOutputComposer: LoginUseCaseOutput {
-    func loginSuceeded() {}
+    let outputs: [LoginUseCaseOutput]
 
-    func loginFailed() {}
+    init(_ outputs: [LoginUseCaseOutput]) {
+        self.outputs = outputs
+    }
+
+    func loginSuceeded() {
+        outputs.forEach { $0.loginSuceeded() }
+    }
+
+    func loginFailed() {
+        outputs.forEach { $0.loginFailed() }
+    }
 }
 
 class LoginUseCaseOutputComposerTests: XCTestCase {
 
     func test_composingZeroOutputs_doesNotCrash() {
-        let sut = LoginUseCaseOutputComposer()
+        let sut = LoginUseCaseOutputComposer([])
         sut.loginSuceeded()
         sut.loginFailed()
+    }
+
+    func test_composingOneOutput_delegateSucceedMessage() {
+        let output1 = LoginUseCaseOutputSpy()
+        let sut = LoginUseCaseOutputComposer([output1])
+
+        sut.loginSuceeded()
+
+        XCTAssertEqual(output1.loginSucceededCallCount, 1)
+        XCTAssertEqual(output1.loginFailedCallCount, 0)
+    }
+
+    func test_composingOneOutput_delegateFailedMessage() {
+        let output1 = LoginUseCaseOutputSpy()
+        let sut = LoginUseCaseOutputComposer([output1])
+
+        sut.loginFailed()
+
+        XCTAssertEqual(output1.loginSucceededCallCount, 0)
+        XCTAssertEqual(output1.loginFailedCallCount, 1)
+    }
+
+    // MARK: Helpers
+
+    private class LoginUseCaseOutputSpy: LoginUseCaseOutput {
+        var loginSucceededCallCount = 0
+        var loginFailedCallCount = 0
+
+        func loginSuceeded() {
+            loginSucceededCallCount += 1
+        }
+
+        func loginFailed() {
+            loginFailedCallCount += 1
+        }
     }
 }
