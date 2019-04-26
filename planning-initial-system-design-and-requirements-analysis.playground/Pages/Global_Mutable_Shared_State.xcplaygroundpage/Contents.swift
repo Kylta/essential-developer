04a6ce91a1d49ba7d24670f1a2dbb8fe73d74b70
Kylta <Christophe.bugnon02@gmail.com>
2019-04-26 19:13:46 +0200

    The problem with this approach is that if we add another screen, we'll need to add a new method in Service and every method could be used from all screen while Feed doesn't care about Login and Login doesn't care about Feed. Another point is if I would move FeedController to another module I can't do this without embedded everything it needs. This could be a hard problem.

--- planning-initial-system-design-and-requirements-analysis.playground/Pages/Global_Mutable_Shared_State.xcplaygroundpage/Contents.swift
+++ planning-initial-system-design-and-requirements-analysis.playground/Pages/Global_Mutable_Shared_State.xcplaygroundpage/Contents.swift
@@ -23,4 +23,3 @@ class LoginViewController: UIViewController {
         }
     }
 }
-
--- planning-initial-system-design-and-requirements-analysis.playground/Pages/Singleton.xcplaygroundpage/Contents.swift
+++ planning-initial-system-design-and-requirements-analysis.playground/Pages/Singleton.xcplaygroundpage/Contents.swift
@@ -3,22 +3,36 @@
 import UIKit
 
 struct LoggedInUser {}
+struct FeedItem {}
 
 // Singleton
 class ApiClient {
-    static let instance = ApiClient()
+    static let shared = ApiClient()
 
     func login(completion: (LoggedInUser) -> Void) {}
+    func loadFeed(completion: ([FeedItem]) -> Void) {}
 }
 
 class MockApiClient: ApiClient {}
 
 class LoginViewController: UIViewController {
-    var api = ApiClient.instance
+    var api = ApiClient.shared
 
     func didTapLoginButton() {
         api.login { user in
             // show next screen
+        }
+    }
+}
+
+class FeedViewController: UIViewController {
+    var api = ApiClient.shared
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        api.loadFeed { loadedItems in
+            // update UI
         }
     }
 }
